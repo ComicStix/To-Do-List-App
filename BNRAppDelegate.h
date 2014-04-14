a72eb98ee20fb3737268b4be7ac24de42bea9433
Nicole Daniels <nzoner12@yahoo.com>
2014-04-14 01:05:46 -0400

    added files

---
+++ BNRAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  BNRAppDelegate.h
+//  iTahDoodle
+//
+//  Created by Nicole Daniels on 3/22/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NSString *BNRDocPath(void);
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (nonatomic) UITableView *taskTable;
+@property (nonatomic) UITextField *taskField;
+@property (nonatomic) UIButton *insertButton;
+
+@property (nonatomic) NSMutableArray *tasks;
+
+- (void)addTask:(id)sender;
+
+@end
